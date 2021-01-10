eda6911a51bba0ac42a63f93bacfa34e1e24fe51
Aaron Konrad <aaron-konrad@t-online.de>
2021-01-10 14:47:57 +0100

    Keine c++ datei mehr

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+	for(int i = 0; i<5;i++){
+		printf("Hello World\n");
+		if(i==3){
+			printf("Hallo Welt(Hi Hi Hi )");
+		}
+		if(i == 4){
+			printf("YO YO yooo YOo");
+		}
+	}
+
+}
