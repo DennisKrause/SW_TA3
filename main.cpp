8796fb09e43e0d3f51a1cd30674244d006550836
DennisKrause <dennisss792@gmail.com>
2021-01-10 13:07:13 +0000

    Aufgabe B12 Ein mehrzeiliges Commentar zur Anderung an der main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-	for(int i = 0; i<6;i++){
+	for(int i = 0; i<5;i++){
 		printf("Hello World\n");
+		if(i==3){
+			printf("Hallo Welt(HiHiHi)");
+		}
 	}
+
 }
